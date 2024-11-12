1b8c785edd36822ea227e05192ca8fcd0020c8f6
Nikolaj <niko6041@hotmail.com>
2024-11-12 12:56:34 +0100

    serverDatabase

--- Backend/server.js
+++ Backend/server.js
@@ -2,53 +2,45 @@ import express from 'express';
 import pg from 'pg';
 import dotenv from 'dotenv';
 
-
 dotenv.config();
 console.log('Connecting to database', process.env.PG_DATABASE);
 const db = new pg.Pool({
-    host:     process.env.PG_HOST,
-    port:     parseInt(process.env.PG_PORT),
+    host: process.env.PG_HOST,
+    port: parseInt(process.env.PG_PORT),
     database: process.env.PG_DATABASE,
-    user:     process.env.PG_USER,
+    user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
+    ssl: process.env.PG_REQUIRE_SSL ? {
+        rejectUnauthorized: false,
+    } : undefined,
 });
-
-const dbResult = await db.query('select now() as now');
+const dbResult = await db.query('select now()');
 console.log('Database connection established on', dbResult.rows[0].now);
 
+const port = process.env.PORT || 3000;
 const server = express();
-const port = 3000;
 
-
-server.use(express.static('Forside'));
-
-
-
-server.use((request, response, next) => {
-    console.log(new Date(), request.method, request.url);
-    next();
-}); 
-
-server.get('/api/allAlbums', onGetAllAlbums);
-
+server.use(express.static('frontend'));
+server.use(onEachRequest);
+server.get('/api/albums', onGetAlbums);
 server.listen(port, onServerReady);
 
+async function onGetAlbums(request, response) {
+    const query = request.query;
+    const start = query.start;
+    const dbResult = await db.query(`
+        select   SELECT * FROM internet_acces
+        where    year($1, 'YYYY') <= year)
+        order by country asc`,
+        [start]);
+    response.send(dbResult.rows);
+}
+
+function onEachRequest(request, response, next) {
+    console.log(new Date(), request.method, request.url);
+    next();
+}
 
 function onServerReady() {
     console.log('Webserver running on port', port);
-    
 }
-
-async function onGetAllAlbums(request,response) {
-    const dbResult = await db.query('select * from albums');
-    response.send(dbResult.rows); 
-}
-
-
-
-
-
-
-
-
-
