cd0a07f299a3cffc86860b22fb556fd9d3e3fc89
MohammadHHaj <muddihhaj@hotmail.com>
2024-11-16 23:00:29 +0100

    Ordnet map.js

--- frontend/map.js
+++ frontend/map.js
@@ -3,14 +3,66 @@
 // Definer kortdataene, inklusive alle lande
 var simplemaps_worldmap_mapdata = {
   main_settings: {
-    width: "responsive",
+    width: "responsive", //'700' or 'responsive'
     background_color: "#FFFFFF",
+    background_transparent: "yes",
     border_color: "#ffffff",
     popups: "detect",
+
+    //State defaults
+    state_description: "#data#% har internet",
     state_color: "#88A4BC",
+    state_hover_color: "off",
+    border_size: "1.5",
+    all_states_inactive: "no",
+    all_states_zoomable: "no",
+
+    //Location defaults
+    location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+
+    //Zoom settings
+    zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+
+    //Advanced settings
     div: "map",
     auto_load: "yes",
-    url_new_tab: "yes",
+    images_directory: "default",
+    fade_time: 0.1,
+    border_hover_color: "gray",
   },
   state_specific: {
     AF: { name: "Afghanistan" },
@@ -224,9 +276,4 @@ var simplemaps_worldmap_mapdata = {
     ZM: { name: "Zambia" },
     ZW: { name: "Zimbabwe" },
   },
-};
-
-// Indlæs kortet efter definitionerne er sat op
-window.onload = function () {
-  simplemaps_worldmap.load();
 };
